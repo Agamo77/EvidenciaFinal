ba0b62b5ecbe547cb8da2123a6b0d3479ac7ff30
Agamo77 <agamo77@hotmail.com>
2025-02-08 09:25:23 -0600

    Update - Clases principales creadas. Siguiente paso probarlas

--- Pacientes.java
+++ Pacientes.java
@@ -1,3 +1,23 @@
 public class Pacientes {
+    private String id;
+    private String nombre;
+
+    public Pacientes(String id, String nombre) {
+        this.id  = id;
+        this.nombre = nombre;
+    }
+
+    public String obtenerId() {
+        return id;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return id + "," + nombre;
+    }
 
 }
